33c25a4d41f9987c8e236151de05cc14cfd7e4e8
Thai Thanh Quan <thaithanhquan11102005@gmail.com>
2026-07-22 22:56:09 +0700

    init backend

---
+++ instahub_api/src/main/java/com/example/instahub_api/InstahubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.instahub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstahubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstahubApiApplication.class, args);
+	}
+
+}
---
+++ instahub_api/src/test/java/com/example/instahub_api/InstahubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instahub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstahubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
